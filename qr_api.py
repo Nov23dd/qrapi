f5c0e21d8e740dab66a8641ab228fd963b02487c
Linnea <linnea.@MacBookAir.hitronhub.home>
2025-02-07 04:12:34 +0800

    Add Procfile and update dependencies

---
+++ qr_api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/generate_qr', methods=['GET'])
+def generate_qr():
+    data = request.args.get('data', '')
+    if not data:
+        return 'No data provided', 400
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+
+    return send_file(img_byte_arr, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
